82258140fe44c1749bd067daba6ec9f83ad52442
John Fell <jfell711@outlook.com>
2022-12-01 12:32:16 -0600

    Practice2 setup

---
+++ Practice2/System/Product.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Practice2.System
+{
+
+
+#region CLASS REQUIREMENTS
+
+/*
+Product #
+
+Contains the name, product id, price, and quantity of each product.
+The price of this product is computed by multiplying the price and the quantity.
+*/
+
+#endregion
+
+public class Product
+{
+
+
+}
+
+}
