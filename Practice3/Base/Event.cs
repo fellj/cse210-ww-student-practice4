810b22bd1e977cc40d1874d66085a0b47b52b391
John Fell <jfell711@outlook.com>
2022-12-08 16:52:20 -0600

    Event class development

--- Practice3/Base/Event.cs
+++ Practice3/Base/Event.cs
@@ -26,9 +26,12 @@ namespace Practice3.Base
     {
 
 
+
         private static string _newLine = Environment.NewLine;
 
         private static string _dividingLine = "###########################";
+
+        private enum EVENTTYPE {Lecture = 1, Outdoor = 2, Reception = 3}
 
         private string _eventTitle;
 
@@ -40,24 +43,108 @@ namespace Practice3.Base
 
         private Address _eventAddress;
 
-        public Event(string inputTitle, string inputDescription, DateTime inputDate, TimeOnly inputTime, Address inputAddress)
+        private int _eventType;
+
+        public Event(string inputTitle, string inputDescription, DateTime inputDate, TimeOnly inputTime, Address inputAddress, int inputEventType)
         {
             _eventTitle       = inputTitle;
             _eventDescription = inputDescription;
             _eventDate        = inputDate;
             _eventTime        = inputTime;
             _eventAddress     = inputAddress;
+            _eventType        = inputEventType;
             
         }
 
         ///<summary>
-        /// Displays the details
+        /// Get the event
+        /// title
+        ///</summary>
+        public string GetEventTitle()
+        {
+            return _eventTitle;
+
+        }
+
+        ///<summary>
+        /// Set the event
+        /// title
+        ///</summary>
+        public void SetEventTitle(string inputEventTitle)
+        {
+            _eventTitle = inputEventTitle;
+
+        }        
+
+        ///<summary>
+        /// Get the event
+        /// date
+        ///</summary>
+        public DateTime GetEventDate()
+        {
+            return _eventDate;
+
+        }
+
+        ///<summary>
+        /// Set the event
+        /// date
+        ///</summary>
+        public void SetEventDate(DateTime inputEventDate)
+        {
+            _eventDate = inputEventDate;
+
+        }                
+
+        ///<summary>
+        /// Get the event
+        /// description
+        ///</summary>
+        public string GetEventDescription()
+        {
+            return _eventDescription;
+
+        }
+
+        ///<summary>
+        /// Set the event
+        /// description
+        ///</summary>
+        public void SetEventDescription(string inputEventDescription)
+        {
+            _eventDescription = inputEventDescription;
+
+        }
+
+        ///<summary>
+        /// Get the event
+        /// description
+        ///</summary>
+        public string GetEventDescription()
+        {
+            return _eventDescription;
+
+        }
+
+        ///<summary>
+        /// Set the event
+        /// description
+        ///</summary>
+        public void SetEventDescription(string inputEventDescription)
+        {
+            _eventDescription = inputEventDescription;
+
+        }
+
+
+        ///<summary>
+        /// Displays the standard details
         /// of the event
         ///</summary>
         public virtual void DisplayStandardEventDetails()
         {
 
-            Console.WriteLine("Event Details");
+            Console.WriteLine("Standard Event Details");
             Console.WriteLine(_dividingLine);
             Console.WriteLine(_newLine);
             Console.WriteLine(_newLine);
@@ -70,6 +157,77 @@ namespace Practice3.Base
 
 
         }
+
+         ///<summary>
+        /// Displays the full details
+        /// of the event
+        ///</summary>
+        public virtual void DisplayFullEventDetails()
+        {
+
+            Console.WriteLine("Full Event Details");
+            Console.WriteLine(_dividingLine);
+            Console.WriteLine(_newLine);
+            Console.WriteLine(_newLine);
+            Console.WriteLine($"Event Title: {_eventTitle}");
+            Console.WriteLine($"Event Type: {GetEventType()}");            
+            Console.WriteLine($"Event Description: {_eventDescription}");
+            Console.WriteLine($"Event Date: {_eventDate}");
+            Console.WriteLine($"Event Time: {_eventTime}");            
+            Console.WriteLine($"Event Address: {_newLine}{_eventAddress.GetFullAddress()}");            
+
+
+
+        }     
+
+         ///<summary>
+        /// Displays a shortened 
+        /// list of the event details
+        ///</summary>
+        public virtual void DisplayShortEventDetails()
+        {
+
+            Console.WriteLine("Short Event Details");
+            Console.WriteLine(_dividingLine);
+            Console.WriteLine(_newLine);
+            Console.WriteLine(_newLine);
+            Console.WriteLine($"Event Title: {_eventTitle}");
+            Console.WriteLine($"Event Type: {GetEventType()}");            
+            Console.WriteLine($"Event Date: {_eventDate}");
+         
+
+
+
+        }         
+
+
+
+        ///<summary>
+        /// Gets the string
+        /// representation of the event
+        /// type based on the enum
+        /// event type.
+        /// The event type is set initially
+        /// in the constructor.
+        ///</summary>
+        public string GetEventType()
+        {
+            switch ((EVENTTYPE)_eventType)
+            {
+                case EVENTTYPE.Lecture:
+                     return "Lecture";
+                
+                case EVENTTYPE.Outdoor:
+                     return "Outdoor";
+
+                case EVENTTYPE.Reception:
+                     return "Reception";
+
+                default:
+                     return "Unknown";
+            }
+
+        }  
 
     }
 
